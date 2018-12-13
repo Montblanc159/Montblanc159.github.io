828d7ad2ae11d509797d67389c39971e51a3192d
Montblanc <guillaume.zaugg@gmail.com>
2018-12-13 12:57:44 +0100

    added PopUp redirection JS

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+function Redirect() {
+  return window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
+
+(confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) ? Redirect() : '';
