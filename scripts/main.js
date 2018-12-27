df404cd0b29c4b4ea907c670e52e8a8ac87c5a89
Montblanc <guillaume.zaugg@gmail.com>
2018-12-27 11:59:45 +0100

    added script for psychedelic mode in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,56 @@ function Redirect() {
 }
 
 (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) ? Redirect() : '';
+
+var background = document.querySelector('body');
+var paragraph1 = document.querySelectorAll('p');
+var headings1 = document.querySelector('h1');
+var headings2 = document.querySelectorAll('h2');
+var headings3 = document.querySelectorAll('h3');
+
+
+function myClickFunction() {
+  document.getElementById("psychedelic").click();
+}
+
+i = true;
+
+function psychedelicMode() {
+
+  if (i === true) {
+    background.className = 'bgpsychedelic';
+    headings1.className = 'headpsychedelic';
+    for (i = 0; i < paragraph1.length; i++) {
+      paragraph1[i].className = "ppsychedelic";
+    }
+
+    for (i = 0; i < headings2.length; i++) {
+      headings2[i].className = "headpsychedelic";
+    }
+
+    for (i = 0; i < headings3.length; i++) {
+      headings3[i].className = "headpsychedelic";
+    }
+
+    return i = false;
+
+  } else {
+
+    background.className = '';
+    headings1.className = '';
+    for (i = 0; i < paragraph1.length; i++) {
+      paragraph1[i].className = "";
+    }
+
+    for (i = 0; i < headings2.length; i++) {
+      headings2[i].className = "";
+    }
+
+    for (i = 0; i < headings3.length; i++) {
+      headings3[i].className = "";
+    }
+
+    return i = true;
+  }
+
+}
